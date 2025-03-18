24e34ebd860889312efa9f1de56e3942ae3b1b71
Shrayash Barnwal <shrayashbarnwal@Shrayashs-MacBook-Air.local>
2025-03-18 23:17:27 +0530

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -15,7 +15,7 @@ const App: React.FC = () => {
   return (
     <div>
       <Header />
-      <Routes>
+      <Routes>  
         <Route path="/" element={<Home />} />
         <Route path="/ct-center" element={<CtCenter />} />
         <Route path="/mri-center" element={<MriCenter />} />
