d506c48c05298eb2d195b5e1d087fbd16314780a
Shrayash Barnwal <shrayashbarnwal@Shrayashs-MacBook-Air.local>
2025-03-27 13:44:19 +0530

    sitemap update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: "https://www.jpradiology.com",
+    }),
+  ],
   base: "/",
-})
+});
